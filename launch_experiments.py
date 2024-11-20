f26bf9961e338b289d76e2026621e38fdf04f775
HichTala <98521878+HichTala@users.noreply.github.com>
2024-11-20 11:46:47 +0100

    Add launching module for python

--- launch_experiments.py
+++ launch_experiments.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 import subprocess
+from email.policy import default
+
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Launch experients')
@@ -9,16 +11,26 @@ def get_args_parser():
     parser.add_argument('--dataset_names', nargs='+', default=["detection-datasets/coco"])
     parser.add_argument('--seed', nargs='+', default=["1338"])
     parser.add_argument('--shots', nargs='+', default=["10"])
-    parser.add_argument('--output_dir', nargs='+', default=["detr-finetuned-coco-10shots"])
+    parser.add_argument('--output_dir', type=str, default="detr-finetuned")
+
+    parser.add_argument('--exec_type', type=str, default="slurm")
 
     return parser
+
 
 def build_cmd(config):
     cmd = ""
     for key, value in config.items():
-        cmd += f"--{key}"
+        cmd += f" --{key} "
         cmd += str(value)
     return cmd
+
+
+def submit_job(cmd, exec_type):
+    if exec_type == "python":
+        cmd = f"python run_object_detection.py{cmd}"
+    subprocess.run(cmd, shell=True)
+
 
 def main(args):
     with open(args.config) as f:
@@ -27,19 +39,15 @@ def main(args):
     for dataset_name in args.dataset_names:
         for seed in args.seed:
             for shots in args.shots:
-                for output_dir in args.output_dir:
-                    config["dataset_name"] = dataset_name
-                    config["seed"] = seed
-                    config["shots"] = shots
-                    config["output_dir"] = output_dir
+                output_dir = f"{args.output_dir}/{dataset_name}/{seed}/{shots}"
+                config["dataset_name"] = dataset_name
+                config["seed"] = seed
+                config["shots"] = shots
+                config["output_dir"] = output_dir
 
-                    cmd = build_cmd(config)
-                    submit_job(cmd)
+                cmd = build_cmd(config)
+                submit_job(cmd, args.exec_type)
 
-
-
-def submit_job(cmd):
-    subprocess.run(["sbatch", "submit.sh", cmd])
 
 if __name__ == "__main__":
     parser = get_args_parser()
