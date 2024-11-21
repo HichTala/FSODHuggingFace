444b85cb2da18fa16d13465616989df76eef2ab6
HichTala <98521878+HichTala@users.noreply.github.com>
2024-11-21 18:22:32 +0100

    Add experiments for all fintening methods

--- launch_experiments.py
+++ launch_experiments.py
@@ -12,7 +12,7 @@ def get_args_parser():
     parser.add_argument('--shots', nargs='+', default=["10"])
     parser.add_argument('--output_dir', type=str, default="detr-finetuned")
 
-    parser.add_argument('--unfreeze_modules', type=str)
+    parser.add_argument('--freeze_modules', type=str)
     parser.add_argument('--freeze_at', type=str)
 
     parser.add_argument('--exec_type', type=str, default="slurm")
@@ -38,18 +38,34 @@ def main(args):
     with open(args.config) as f:
         config = json.load(f)
 
+    if not args.freeze_modules:
+        args.freeze_modules = [[], ['backbone'], ['backbone'], ['bias'], ['norm']]
+    if args.freeze_at:
+        args.freeze_at = [[], ['0'], ['half'], [], []]
+
     for dataset_name in args.dataset_names:
         for seed in args.seed:
-            for shots in args.shots:
-                output_dir = f"{args.output_dir}/{dataset_name.split('/')[-1]}/{seed}/{shots}"
+            for shot in args.shots:
+                for freeze_modules, freeze_at in zip(args.freeze_modules, args.freeze_at):
+                    output_dir = f"{args.output_dir}/{dataset_name.split('/')[-1]}/{seed}/{shot}/"
 
-                config["dataset_name"] = dataset_name
-                config["seed"] = seed
-                config["shots"] = shots
-                config["output_dir"] = output_dir
+                    if len(freeze_modules) == 0:
+                        output_dir += "full_finetuning"
+                    for freeze_module, freeze_at_single in zip(freeze_modules, freeze_at):
+                        if output_dir[-1] != '/':
+                            output_dir += '_'
+                        output_dir += f"{freeze_module}-{freeze_at_single}" if freeze_at_single != '0' else f"{freeze_module}-full"
 
-                cmd = build_cmd(config)
-                submit_job(cmd, exec_type=args.exec_type)
+                    config['freeze_modules'] = freeze_modules
+                    config['freeze_at'] = freeze_at
+
+                    config["dataset_name"] = dataset_name
+                    config["seed"] = seed
+                    config["shots"] = shot
+                    config["output_dir"] = output_dir
+
+                    cmd = build_cmd(config)
+                    submit_job(cmd, exec_type=args.exec_type)
 
 
 if __name__ == "__main__":
