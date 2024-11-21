6c2d929be10bba20be31fdbea4c9fba9afc1818b
Hich Tala <98521878+HichTala@users.noreply.github.com>
2024-11-21 11:01:31 +0100

    Add python exec_type and correct output_dir

--- launch_experiments.py
+++ launch_experiments.py
@@ -9,14 +9,16 @@ def get_args_parser():
     parser.add_argument('--dataset_names', nargs='+', default=["detection-datasets/coco"])
     parser.add_argument('--seed', nargs='+', default=["1338"])
     parser.add_argument('--shots', nargs='+', default=["10"])
-    parser.add_argument('--output_dir', nargs='+', default=["detr-finetuned-coco-10shots"])
+    parser.add_argument('--output_dir', type=str, default="detr-finetuned")
+
+    parser.add_argument('--exec_type', type=str, default="slurm")
 
     return parser
 
 def build_cmd(config):
     cmd = ""
     for key, value in config.items():
-        cmd += f"--{key}"
+        cmd += f" --{key} "
         cmd += str(value)
     return cmd
 
@@ -27,19 +29,22 @@ def main(args):
     for dataset_name in args.dataset_names:
         for seed in args.seed:
             for shots in args.shots:
-                for output_dir in args.output_dir:
-                    config["dataset_name"] = dataset_name
-                    config["seed"] = seed
-                    config["shots"] = shots
-                    config["output_dir"] = output_dir
+                output_dir = f"{args.output_dir}/{dataset_name.split('/')[-1]}/{seed}/{shots}"
 
-                    cmd = build_cmd(config)
-                    submit_job(cmd)
+                config["dataset_name"] = dataset_name
+                config["seed"] = seed
+                config["shots"] = shots
+                config["output_dir"] = output_dir
+
+                cmd = build_cmd(config)
+                submit_job(cmd, exec_type=args.exec_type)
 
 
 
-def submit_job(cmd):
-    subprocess.run(["sbatch", "submit.sh", cmd])
+def submit_job(cmd, exec_type):
+    if exec_type == "python":
+        cmd = f"python run_object_detection.py{cmd}"
+    subprocess.run(cmd, shell=True)
 
 if __name__ == "__main__":
     parser = get_args_parser()
