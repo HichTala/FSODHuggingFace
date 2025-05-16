e046ecfa8493ec73428bcdd84785e14e8db35520
HichTala <98521878+HichTala@users.noreply.github.com>
2025-05-16 13:43:55 +0200

    Change old evaluation strategy to new eval strategy

--- launch_experiments.py
+++ launch_experiments.py
@@ -88,7 +88,7 @@ def main(args):
             config["dataset_name"] = dataset_name
             config["output_dir"] = output_dir
             config["logging_strategy"] = "epoch"
-            config["evaluation_strategy"] = "epoch"
+            config["eval_strategy"] = "epoch"
             config["save_strategy"] = "epoch"
 
             cmd = build_cmd(config)
